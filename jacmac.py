82f07463864985af3b67090e6d3673c16375f0d3
jaclu <jacob.lundqvist@gmail.com>
2023-01-22 13:37:49 +0100

    increased min display for packet-loss

--- jacmac.py
+++ jacmac.py
@@ -19,6 +19,7 @@
 import sys
 
 from sb.sb_local import SB
+# from sb.sb_muted import SB
 
 
 class JacMacConfig(SB):
@@ -40,11 +41,12 @@ class JacMacConfig(SB):
             set -g @packet-loss-history_size "7"
             set -g @packet-loss_weighted_average "1"
 
-            # set -g @packet-loss_level_disp "0.1"
+            set -g @packet-loss_level_disp "3.4"
             set -g @packet-loss_level_alert "17"
             set -g @packet-loss_level_crit "40"
 
-            # set -g @packet-loss_color_alert "colour181"
+            set -g @packet-loss_color_alert "colour181"
+            set -g @packet-loss_color_alert "yellow"
             # set -g @packet-loss_color_crit "red"
             # set -g @packet-loss_color_bg "black"
 
