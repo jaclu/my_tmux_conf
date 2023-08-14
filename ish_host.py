fdd941d69007f6db7b3694c14e2ac7c5fe02de64
jaclu <jacob.lundqvist@gmail.com>
2023-08-14 11:50:12 +0200

    updated ipads

--- ish_host.py
+++ ish_host.py
@@ -35,7 +35,7 @@ from sb.sb_muted import SB
 class ishHost(SB):
     # status_interval = 5
 
-    # plugin_handler = "manual"
+    plugin_handler = ""
 
     # use_embedded_scripts = False
 
@@ -45,7 +45,7 @@ class ishHost(SB):
     #  Thereby not making them available for iSH hosts
     #
 
-    def plugin_packet_loss(self) -> list:  # 1.9
+    def not_plugin_packet_loss(self) -> list:  # 1.9
         if os.path.isfile("/etc/debian_version"):
             # Ish Debian tends to fail on this plugin on my (oldish) iPads
             min_vers = 99.0
@@ -72,6 +72,27 @@ class ishHost(SB):
             set -g @packet-loss-suffix |
             """,
         ]
+
+    def plugin_better_mouse_mode(self) -> list:  # 2.1
+        return ("jaclu/tmux-better-mouse-mode", 99, "")
+
+    def plugin_menus(self) -> list:  # 1.8
+        return ("6", 99, "")
+
+    def plugin_power_zoom(self) -> list:  # 2.0
+        return ("5", 99, "")
+
+    def plugin_prefix_highlight(self) -> list:  # 2.0
+        return ("4", 99, "")
+
+    def plugin_resurrect(self) -> list:  # 1.9
+        return ("3", 99, "")
+
+    def plugin_suspend(self) -> list:  # 2.4
+        return ("2", 99, "")
+
+    def plugin_yank(self) -> list:  # 1.8
+        return ("1", 99, "")
 
 
 if __name__ == "__main__":
