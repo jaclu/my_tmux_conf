0e4c9d3dcec58d29e5511f57f1d253dd9e7c12e9
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2024-02-21 17:32:16 +0100

    uses sb style

--- hetz1.py
+++ hetz1.py
@@ -8,16 +8,7 @@
 #  Template giving the status bar the colors I use for a cloud host
 #
 
-from default_plugins import DefaultPlugins
-
-
-class Hetz1(DefaultPlugins):
-    def status_bar_customization(self, print_header=True):
-        super().status_bar_customization(print_header=print_header)
-        if self.vers_ok("1.9"):
-            self.write("set -g status-style fg=black,bg=colour44")
-        return print_header  # request footer to be printed
-
+from sb.sb_cloud import SB
 
 if __name__ == "__main__":
-    Hetz1().run()
+    SB().run()
