cb4a21cc3f06f8e204f510f911e6da92d90e1831
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2024-04-17 14:21:33 +0200

    tweaked hosts

--- hetz1.py
+++ hetz1.py
@@ -12,5 +12,37 @@
 
 from sb.sb_cloud import SB
 
+
+class Hetz1(SB):
+    """hetz1 config"""
+
+    status_interval = 5
+
+    def plugin_packet_loss(self):  # 1.9
+        min_vers = 1.9
+        if self.is_tmate():
+            min_vers = 99  # disable for tmate
+        return [
+            "jaclu/tmux-packet-loss",
+            min_vers,
+            """
+            set -g @packet-loss-ping_count    6
+            set -g @packet-loss-history_size 6
+
+            set -g @packet-loss-display_trend     1
+            set -g @packet-loss-hist_avg_display  1
+
+            set -g @packet-loss-level_alert      26
+
+            set -g @packet-loss-color_alert colour21
+            set -g @packet-loss-color_bg    colour226
+
+            set -g @packet-loss-prefix '|'
+            set -g @packet-loss-suffix '|'
+            """,
+        ]
+
+
 if __name__ == "__main__":
     SB().run()
+    # Hetz1().run()
