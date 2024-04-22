ca7f87c48597cdcc3312e5ee866265dcdb299f40
jaclu <jacob.lundqvist@gmail.com>
2024-04-23 00:34:34 +0200

    conf

--- t2.py
+++ t2.py
@@ -66,10 +66,10 @@ class T2(IshHost, SB):
             "jaclu/tmux-packet-loss",
             min_vers,
             """
-            set -g @packet-loss-ping_host 8.8.8.8
+            set -g @packet-loss-ping_host 1.1.1.1
 
-            set -g @packet-loss-display_trend     true
-            set -g @packet-loss-hist_avg_display  true
+            set -g @packet-loss-display_trend     yes
+            set -g @packet-loss-hist_avg_display  yes
 
             set -g @packet-loss-color_alert colour21
             set -g @packet-loss-color_bg    colour226
@@ -77,7 +77,6 @@ class T2(IshHost, SB):
             set -g @packet-loss-prefix |
             set -g @packet-loss-suffix |
 
-            set -g @packet-loss-use_param_cache false
             """,
         ]
 
