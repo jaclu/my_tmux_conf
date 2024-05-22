a2e2b208283a5cc299964d2cdda4b8ea4bb4bbea
jaclu <jacob.lundqvist@gmail.com>
2024-05-22 14:58:45 +0200

    jacmac packetloss

--- jacmac.py
+++ jacmac.py
@@ -42,9 +42,9 @@ class JacMacConfig(SB):
             min_vers,
             """
             set -g @packet-loss-ping_host 8.8.8.8
-            set -g @packet-loss-ping_count     7
-            set -g @packet-loss-history_size   5
-            set -g @packet-loss-level_alert 15 # 4-26 6-17 7-15
+            set -g @packet-loss-ping_count     6
+            set -g @packet-loss-history_size   6
+            set -g @packet-loss-level_alert 18 # 4-26 6-17 7-15
 
             set -g @packet-loss-weighted_average  yes
             set -g @packet-loss-display_trend     no
