73397d683bf902bde546e8d83c107071124c0d09
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2024-09-07 14:52:22 +0200

    linting

--- progress-venv-build.sh
+++ progress-venv-build.sh
@@ -8,6 +8,7 @@
 #  size aprox 29M and file count: 1520
 #
 while true; do
-    printf "size: %s   -  file count:  %s\n" $(du -sh .venv/ | cut -f1) $(find .venv | wc -l)
+    printf "size: %s   -  file count:  %s\n" \
+	   "$(du -sh .venv/ | cut -f1)" "$(find .venv | wc -l)"
     sleep 1
 done
