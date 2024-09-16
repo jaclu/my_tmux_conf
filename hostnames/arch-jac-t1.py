cb098b52dfa53870e2e37ec86f2de80f9eb799de
jaclu <jacob.lundqvist@gmail.com>
2024-09-16 12:02:33 +0000

    added sl-test1

--- hostnames/arch-jac-t1.py
+++ hostnames/arch-jac-t1.py
@@ -1,1 +1,26 @@
-arch-jac-t2.py
+#!/usr/bin/env python3
+#
+#  Copyright (c) 2022-2024: Jacob.Lundqvist@gmail.com
+#  License: MIT
+#
+#  Part of https://github.com/jaclu/my_tmux_conf
+#
+#  Template giving the status bar the colors I use for a cloud host
+#
+
+""" host hetz1 """
+
+import os
+import sys
+
+# Put the "project path first to support relative imports"
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+sys.path.insert(0, root_dir)
+
+# flake8: noqa: E402
+# pylint: disable=wrong-import-position
+from sb.sb_tst import SB
+
+
+if __name__ == "__main__":
+    SB().run()
