30b32b33e15a1ed743490fdd7b4403701028b939
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2025-01-09 12:20:06 +0100

    linting

--- tmate.py
+++ tmate.py
@@ -26,7 +26,7 @@ class Tmate(SB):
 
     def status_bar_customization(self, print_header: bool = True) -> bool:
         super().status_bar_customization(print_header=print_header)
-        #if self.prefix_key.lower() != "c-b":
+        # if self.prefix_key.lower() != "c-b":
         tag = f"#[fg=green,bg=black]{self.prefix_key} #[default]"
         self.sb_left = f"{tag}{self.sb_left}"
 
