60735c408fcb3efac16b33ceba7c4396c43b960a
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2025-03-17 13:48:13 +0100

    better explain

--- base.py
+++ base.py
@@ -1476,7 +1476,7 @@ class BaseConfig(TmuxConfig):
     def muc_non_default_prefix(self, default):
         # If a non-default is used, display it as a prefix
         if default != self.muc_keys[default]:
-            return f"Use key: {default} | "
+            return f"(Use key: {default} ) "
         return ""
 
     def auc_meta_ses_handling(self):
