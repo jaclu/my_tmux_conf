5427b39756cfaff9ad2a02ac2b768d55d66e5741
jaclu <jacob.lundqvist@gmail.com>
2025-05-29 23:23:51 +0200

    tablet_kbd work

--- tablet_kbd.py
+++ tablet_kbd.py
@@ -500,8 +500,10 @@ class IshConsole(LimitedKbdSpecialHandling):
                 #    Special case to avoid cutof at second -N
                 #    on tmux < 3.1
                 w(f'bind -N "Enables M-N" -n  User{k2uk[key]}   send-keys    {key}')
-            elif key in ('M-"', "M-{", "M-}"):
+            elif key == 'M-"':
                 w(f"""bind -N 'Send {key}' -n User{k2uk[key]}     send-keys     '{key}' """)
+            elif key in ("M-{", "M-}"):
+                w(f"""bind -N "Send {key}" -n User{k2uk[key]}     send-keys     "{key}" """)
             else:
                 w(f'bind -N "Send {key}" -n User{k2uk[key]}     send-keys     {key}')
         w()  # spacer line
