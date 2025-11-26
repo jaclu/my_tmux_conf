66df2bd7118c178328cf4d9e98475a6f2e11cc5b
jaclu <jacob.lundqvist@gmail.com>
2025-11-26 23:03:52 +0100

    updated default tmux-profiles Styling

--- default_plugins.py
+++ default_plugins.py
@@ -301,10 +301,10 @@ class DefaultPlugins(BaseConfig):
 
             set -g @menus_format_title "'#[fg=yellow,align=left] #{@menu_name} '"
             set -g @menus_simple_style_border "fg=green,bg=default"
+            set -g @menus_border_type 'rounded'
             set -g @menus_nav_next "#[fg=colour220]-->"
             set -g @menus_nav_prev "#[fg=colour71]<--"
             set -g @menus_nav_home "#[fg=colour84]<=="
-            set -g @menus_border_type 'rounded'
 
             # set -g @menus_location_x C
             # set -g @menus_location_y C
@@ -314,14 +314,6 @@ class DefaultPlugins(BaseConfig):
 
             set -g @menus_use_hint_overlays No
             # set -g @menus_show_key_hints Yes
-
-            #
-            #  Slightly catppuccin frappe inspired
-            #
-            # fg @thm_surface_0 bg @thm_yellow
-            # set -g @menus_simple_style_selected "fg=#414559,bg=#e5c890"
-            # set -g @menus_simple_style "bg=#414559"        # @thm_surface_0
-            # set -g @menus_simple_style_border "bg=#414559" # @thm_surface_0
 
             # set -g @menus_log_file "$HOME/tmp/tmux-menus.log"
             """,
