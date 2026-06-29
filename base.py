b843cdf7a2170a662f42bf59e8067d585a5097be
jaclu <jacob.lundqvist@gmail.com>
2026-06-29 12:45:15 +0200

    Explain highlighting when prompting if server should be killed

--- base.py
+++ base.py
@@ -1846,6 +1846,8 @@ if-shell -F '#{||:#{==:#{window_panes},1},#{!=:#{window_zoomed_flag},#{@zoom-sta
             s += 'command-prompt -p "'
         s += "Kill this server "
         if self.vers_ok(3.3):
+            # Highlight conf-file and hostname, to make it absolutely clear
+            # what tmux server will be killed
             s += f"#[reverse]{self.conf_file}#[default] [#[reverse]#h#[default]]"
         else:
             s += f"{self.conf_file}"
@@ -1908,7 +1910,8 @@ if-shell -F '#{||:#{==:#{window_panes},1},#{!=:#{window_zoomed_flag},#{@zoom-sta
     def mkscript_toggle_mouse(self):
         """Toggles mouse handling on/off"""
         #  The {} encapsulating the script needs to be doubled to escape them
-        toggle_mouse_sh = [f"""
+        toggle_mouse_sh = [
+            f"""
 {self._fnc_toggle_mouse}() {{
     #  This is so much easier to do in a proper script...
     old_state=$($TMUX_BIN show -gv mouse)
@@ -1919,7 +1922,8 @@ if-shell -F '#{||:#{==:#{window_panes},1},#{!=:#{window_zoomed_flag},#{@zoom-sta
     fi
     $TMUX_BIN {self.opt_ses} mouse $new_state
     $TMUX_BIN display-message "mouse: $new_state"
-}}"""]
+}}"""
+        ]
         self.es.create(self._fnc_toggle_mouse, toggle_mouse_sh)
 
     def mkscript_shlvl_offset(self):
@@ -2086,7 +2090,8 @@ timer_end() {{
         # self.sb_purge_tpm_running = f"$TMUX_BIN {self.opt_ses} -q status-right "
         # \\"$($TMUX_BIN display-message -p '#{{status-right}}' | sed 's/{purge_seq}//')\\"
 
-        clear_tpm_init_sh = [f"""
+        clear_tpm_init_sh = [
+            f"""
 {self._fnc_tpm_indicator}() {{
     #
     # Function that turns on/off self.tpm_initializing addition to status-right
@@ -2124,12 +2129,11 @@ timer_end() {{
         $TMUX_BIN setenv -gu {self.tpm_working_incicator}
     fi
 }}
-"""]
+"""
+        ]
         self.es.create(self._fnc_tpm_indicator, clear_tpm_init_sh)
 
-    def incompatible_tmux_conf(
-        self, lib_vers_found: str, reason: str, details: str = ""
-    ):
+    def incompatible_tmux_conf(self, lib_vers_found: str, reason: str, details: str = ""):
         print()
         print("ERROR: Incompatible tmux-conf package")
         print()
@@ -2149,9 +2153,7 @@ timer_end() {{
             return  # user keys not yet available
         if sequence[:1] != "\\":
             print()
-            print(
-                f"ERROR: alternate_key_euro({sequence}) must be given in octal notation"
-            )
+            print(f"ERROR: alternate_key_euro({sequence}) must be given in octal notation")
             sys.exit(mtc_utils.ERROR_USER_KEY_NOT_OCTAL)
 
         w = self.write
