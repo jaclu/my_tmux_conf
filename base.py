a04727a9cd76e5031969c42e646d36bf6c5cdc29
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2026-08-15 00:01:10 +0200

    import sorting

--- base.py
+++ base.py
@@ -52,7 +52,6 @@ import sys
 
 # Ruff import ordering is authoritative here; no blank line after __main__
 import __main__
-
 import mtc_utils
 from tablet_kbd import special_consoles_config
 from tmux_conf import TmuxConfig
