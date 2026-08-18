2f3d7bcf5c479ec51ec2360adf8d1dc015eb6422
Jacob Lundqvist <Jacob.Lundqvist@gmail.com>
2026-08-18 21:27:27 +0200

    black-linting

--- base.py
+++ base.py
@@ -1397,8 +1397,7 @@ class BaseConfig(TmuxConfig):
                 delay = "-d 400"
             else:
                 delay = ""
-            w(
-                f"""
+            w(f"""
                 #{
                     (
                         "Displays that tmux picked up clipboard and (hopefully) "
@@ -1407,8 +1406,7 @@ class BaseConfig(TmuxConfig):
                 }
                 set-hook -g pane-set-clipboard{idx} "display-message {delay} '{
                     msg
-                }'" """
-            )
+                }'" """)
 
         if self.vers_ok(v_min_smart_hooks) and not self.is_tmate():
             idx = self.get_next_hook_array_idx()
@@ -1935,8 +1933,7 @@ if-shell -F '#{||:#{==:#{window_panes},1},#{!=:#{window_zoomed_flag},#{@zoom-sta
     def mkscript_toggle_mouse(self):
         """Toggles mouse handling on/off"""
         #  The {} encapsulating the script needs to be doubled to escape them
-        toggle_mouse_sh = [
-            f"""
+        toggle_mouse_sh = [f"""
 {self._fnc_toggle_mouse}() {{
     #  This is so much easier to do in a proper script...
     old_state=$($TMUX_BIN show -gv mouse)
@@ -1947,8 +1944,7 @@ if-shell -F '#{||:#{==:#{window_panes},1},#{!=:#{window_zoomed_flag},#{@zoom-sta
     fi
     $TMUX_BIN {self.opt_ses} mouse $new_state
     $TMUX_BIN display-message "mouse: $new_state"
-}}"""
-        ]
+}}"""]
         self.es.create(self._fnc_toggle_mouse, toggle_mouse_sh)
 
     def mkscript_shlvl_offset(self):
@@ -2115,8 +2111,7 @@ timer_end() {{
         # self.sb_purge_tpm_running = f"$TMUX_BIN {self.opt_ses} -q status-right "
         # \\"$($TMUX_BIN display-message -p '#{{status-right}}' | sed 's/{purge_seq}//')\\"
 
-        clear_tpm_init_sh = [
-            f"""
+        clear_tpm_init_sh = [f"""
 {self._fnc_tpm_indicator}() {{
     #
     # Function that turns on/off self.tpm_initializing addition to status-right
@@ -2154,8 +2149,7 @@ timer_end() {{
         $TMUX_BIN setenv -gu {self.tpm_working_incicator}
     fi
 }}
-"""
-        ]
+"""]
         self.es.create(self._fnc_tpm_indicator, clear_tpm_init_sh)
 
     def incompatible_tmux_conf(
